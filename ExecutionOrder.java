f4003b40462c983cf53d277b769eef40b2ba77a2
premsla <premalatha.p2022ai-ds@sece.ac.in>
2026-01-28 13:20:47 +0530

    Added Paren child execution order

---
+++ ExecutionOrder.java
@@ -0,0 +1,17 @@
+class Parent{
+    static {
+        System.out.println("Parent static block");
+    }
+}
+class Child extends Parent{
+    static {
+        System.out.println("Child static block");
+    }
+}
+public class ExecutionOrder {
+    //static Child c = new Child(); 
+    public static void main(String[] args) {
+        System.out.println("Main method");
+        new Child();
+    }
+}
